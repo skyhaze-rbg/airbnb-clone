4952792e7cbea46a08e6dc8d831439d742ad3b85
Akash Gurung <58412114+skyhaze-rbg@users.noreply.github.com>
2024-04-27 11:10:24 +0800

    ip

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+import PropertyListings from "@/app/components/propertyListings";
+
+export default function Home() {
+
+    return (
+        <main>
+            <PropertyListings/>
+        </main>
+    );
+}
